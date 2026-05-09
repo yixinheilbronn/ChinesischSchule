e5a96bcc895da5c5510ff53087a2f65f19ebabe8
yixinheilbronn <yixinheilbronn@gmail.com>
2026-05-09 11:01:17 +0200

    Configure image remotePatterns for Vercel Blob
    Add remotePatterns to allow images from Vercel Blob.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   poweredByHeader: false,
+  
+ /** Vercel Blob as white list*/
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "kbwh6t8wdcimiyna.public.blob.vercel-storage.com",
+        port: "",
+        pathname: "/**", //
+      },
+    ],
+  },
+  
   async headers() {
     return [
       {
